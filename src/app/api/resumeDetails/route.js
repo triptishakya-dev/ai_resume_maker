93023442d6f8fe6728c7b79e5446c577181a839e
triptishakya-dev <triptishakya.2001@gmail.com>
2026-01-08 02:16:46 +0530

    feat: Introduce resume upload page and API for submitting job application details.

--- src/app/api/resumeDetails/route.js
+++ src/app/api/resumeDetails/route.js
@@ -1,25 +1,32 @@
-import { startTurbopackTraceServer } from "next/dist/build/swc/generated-native";
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function POST(req) {
   try {
     const formData = await req.formData();
-    const CompanyName = FormData.get("CompanyName");
-    const jobTitle = FormData.get("jobTitle");
-    const jobDescription = FormData.get("jobDescription");
-    const resume = formData.get("resume");
+    console.log(formData)
+    const CompanyName = formData.get("company-name");
+    const jobTitle = formData.get("job-title");
+    const jobDescription = formData.get("job-discription");
+    const resume = formData.get("file");
+
+    console.log(CompanyName, jobTitle, jobDescription, resume);
+
 
     if (!CompanyName || !jobTitle || !jobDescription || !resume) {
-      return NextRequest.json(
-        { error: "internal server error" },
-        { status: 500 }
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 }
       );
     }
 
+    return NextResponse.json(
+      { success: "success" },
+      { status: 200 }
+    );
 
   } catch (error) {
     console.log(error);
-    return NextRequest.json(
+    return NextResponse.json(
       { error: "internal server error" },
       { status: 500 }
     );
